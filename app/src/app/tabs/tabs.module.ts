254702265f0f782df3debc821fa67d9f861b7e7c
Phoebe <phoebek@stanford.edu>
2020-03-13 02:05:26 -0700

    Add tabs page with routing

---
+++ app/src/app/tabs/tabs.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children:[
+        { path: 'home', loadChildren: '../home/home.module#HomePageModule' },
+        { path: 'timeline', loadChildren: './timeline/timeline.module#TimelinePageModule' },
+        { path: 'about', loadChildren: '../about/about.module#AboutPageModule' },
+    ]
+  },
+  {
+    path:'',
+    redirectTo:'/tabs/home',
+    pathMatch:'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
